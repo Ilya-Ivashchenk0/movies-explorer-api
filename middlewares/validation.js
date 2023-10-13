f90097103efaf89d85810e4ef605ec60966eb786
Ilya-Ivashchenk0 <donhuilio@yandex.ru>
2023-10-13 09:39:15 +0300

    next

--- middlewares/validation.js
+++ middlewares/validation.js
@@ -3,7 +3,8 @@ const { celebrate, Joi, Segments } = require('celebrate')
 const validateUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().min(2).max(30),
-    name: Joi.string().required().min(2).max(30)
+    name: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(8)
   })
 })
 
